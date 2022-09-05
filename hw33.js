8e591c9a62d4b0c54e93d672680b7248d0602f0b
aohanesian <103946712+aohanesian@users.noreply.github.com>
2022-09-05 18:29:02 +0300

    init

---
+++ hw33.js
@@ -0,0 +1,20 @@
+`use strict`;
+
+(function () {
+    const ladder = {
+        step: 0,
+        up: function () {
+            this.step++;
+            return this;
+        },
+        down: function () {
+            this.step--;
+            return this;
+        },
+        showStep: function () { // показывает текущую ступеньку
+            console.log((this.step));
+            return this;
+        }
+    };
+    ladder.up().up().down().showStep(); // 1
+})()
